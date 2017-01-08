84a61903c1b585fac0a451d9aaa28c8e8542bb9e
Andrey Tyapugin <andreylcm@gmail.com>
2017-01-08 19:53:51 +0300

    add php

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="ya-title=#03A9F4,ya-dock=#03A9F4">
+    <meta name="theme-color" content="#03A9F4">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="description" content="Портал создан для помощи абонентам в поиске информации по продукции Триколор ТВ, 
+      а так же для помощи в настройке оборудования">
+    <meta name="keywords" content="триколор, тв, приемник, ресивер, гс, gs, general satellite, настройка, каналы, спутник">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Триколор ТВ - Инфо</title>
+  </head>
+  <body>
+    <div class="wrapper">
+      <header class="header">
+        <div class="layout">
+          <div class="top-panel"></div>
+          <div class="logo-container">
+            <div class="logo__box">
+              <img class="logo" src="img/logo.svg" alt="Logo" width="50" height="50"> 
+              <h1 class="site__name">триколор<span class="site__name--red">тв</span></h1>
+              <span class="site__name-description">информационный портал</span>
+            </div>
+            <div class="search-container">
+              <input class="top-search" type="search" placeholder="Поиск">
+            </div>
+            <div class="hamburger"></div>
+          </div>
+          <nav class="top-nav height-none">
+            <ul class="main-menu">
+              <li class="main-menu__item"><a class="main-menu__link" href="#1">Обмен</a></li>
+              <li class="main-menu__item"><a class="main-menu__link" href="#2">Поддержка</a></li>
+              <li class="main-menu__item"><a class="main-menu__link" href="#3">Инструкции</a></li>
+              <li class="main-menu__item"><a class="main-menu__link" href="#4">Сервис</a></li>
+              <li class="main-menu__item"><a class="main-menu__link" href="#4">Помощь</a></li>
+              <li class="main-menu__item"><a class="main-menu__link" href="#4">О нас</a></li>  
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <div class="content">
+        <div class="layout breadcrumbs">
+          <a class="breadcrumbs__link" href="#bread">Главная /</a>
+          <a class="breadcrumbs__link" href="#bread">Лента /</a>        
+        </div>
+        <div class="layout flex">
+          <main class="feed flex">
+            <div class="post-container">
+              <div class="post-image">
+                <img src="img/post_1.jpg" alt="Ultra HD" width="100%">
+              </div>
+              <div class="post-body">
+                <h2 class="post__title">Новое обновление для приемника GS B520</h2>
+                <div class="post__meta meta--show">
+                  <time class="post__time" datetime="2016-12-21" title="Дата публикации">21.12.2016</time>
+                  <div class="post__category"><a class="post__category-link" href="#category" title="В разделе">Ресиверы</a></div>
+                </div>
+                <div class="post__preview-text text--show">
+                  Загрузка...
+                </div>
+                <div class="post__stats stats--show">
+                  <!--<div class="post__views"></div>
+                  <div class="post__comments"></div>-->
+                  <a class="read__more" href="#readmore">далее</a>
+                </div>
+              </div>
+            </div>
+            <div class="post-container">
+              <div class="post-image">
+                <img src="img/post_3.png" alt="Tablet" width="100%">
+              </div>
+              <div class="post-body">
+                <h2 class="post__title">Зачем менять оборудование?</h2>
+                <div class="post__meta meta--show">
+                  <time class="post__time" datetime="2016-12-20" title="Дата публикации">19.12.2016</time>
+                  <div class="post__category"><a class="post__category-link" href="#category" title="В разделе">Новости</a></div>
+                </div>
+                <div class="post__preview-text text--show">
+                  Для абсолютно всех всех абонентов «Триколор ТВ», владельцев устаревших приёмников, действуют специальные предложения обмена оборудования на современное, поддерживающее формат HD.
+                  Благодаря специальным акциям переход на новое оборудование обойдется вам значительно дешевле, чем приобретение нового комплекта за полную стоимость.
+                </div>
+                <div class="post__stats stats--show">
+                  <!--<div class="post__views">115</div>
+                  <div class="post__comments"></div>-->
+                  <a class="read__more" href="#readmore">далее</a>
+                </div>
+              </div>
+            </div>
+            <div class="post-container">
+              <div class="post-image">
+                <img src="img/post_4.jpg" alt="Tablet" width="100%">
+              </div>
+              <div class="post-body">
+                <h2 class="post__title">Мультбиблиотека от пакета "Детский"!</h2>
+                <div class="post__meta meta--show">
+                  <time class="post__time" datetime="2016-12-20" title="Дата публикации">19.12.2016</time>
+                  <div class="post__category"><a class="post__category-link" href="#category" title="В разделе">Новости</a></div>
+                </div>
+                <div class="post__preview-text text--show">
+                  Для абсолютно всех всех абонентов «Триколор ТВ», владельцев устаревших приёмников, действуют специальные предложения обмена оборудования на современное, поддерживающее формат HD.
+                  Благодаря специальным акциям переход на новое оборудование обойдется вам значительно дешевле, чем приобретение нового комплекта за полную стоимость.
+                </div>
+                <div class="post__stats stats--show">
+                  <!--<div class="post__views">115</div>
+                  <div class="post__comments"></div>-->
+                  <a class="read__more" href="#readmore">далее</a>
+                </div>
+              </div>
+            </div>
+            <div class="post-container">
+              <div class="post-image">
+                <img src="img/post_5.jpg" alt="Tablet" width="100%">
+              </div>
+              <div class="post-body">
+                <h2 class="post__title">Спутниковый интернет</h2>
+                <div class="post__meta meta--show">
+                  <time class="post__time" datetime="2016-12-20" title="Дата публикации">19.12.2016</time>
+                  <div class="post__category"><a class="post__category-link" href="#category" title="В разделе">Новости</a></div>
+                </div>
+                <div class="post__preview-text text--show">
+                  Для абсолютно всех всех абонентов «Триколор ТВ», владельцев устаревших приёмников, действуют специальные предложения обмена оборудования на современное, поддерживающее формат HD.
+                  Благодаря специальным акциям переход на новое оборудование обойдется вам значительно дешевле, чем приобретение нового комплекта за полную стоимость.
+                </div>
+                <div class="post__stats stats--show">
+                  <!--<div class="post__views">115</div>
+                  <div class="post__comments"></div>-->
+                  <a class="read__more" href="#readmore">далее</a>
+                </div>
+              </div>
+            </div>
+            <div class="post-container">
+              <div class="post-image">
+                <img src="img/post_3.jpg" alt="Tablet" width="100%">
+              </div>
+              <div class="post-body">
+                <h2 class="post__title">Акция 1000 фильмов в подарок</h2>
+                <div class="post__meta meta--show">
+                  <time class="post__time" datetime="2016-12-20" title="Дата публикации">19.12.2016</time>
+                  <div class="post__category"><a class="post__category-link" href="#category" title="В разделе">Новости</a></div>
+                </div>
+                <div class="post__preview-text text--show">
+                  Для абсолютно всех всех абонентов «Триколор ТВ», владельцев устаревших приёмников, действуют специальные предложения обмена оборудования на современное, поддерживающее формат HD.
+                  Благодаря специальным акциям переход на новое оборудование обойдется вам значительно дешевле, чем приобретение нового комплекта за полную стоимость.
+                </div>
+                <div class="post__stats stats--show">
+                  <!--<div class="post__views">115</div>
+                  <div class="post__comments"></div>-->
+                  <a class="read__more" href="#readmore">далее</a>
+                </div>
+              </div>
+            </div>
+          </main>
+          <aside class="sidebar">
+            <div class="popular-block">
+              <h2 class="popular__title">Популярные записи</h2>          
+            </div>
+          </aside>
+        </div>
+      </div>
+      <footer class="footer">
+        <div class="layout flex">
+          <div class="footer-block">
+            <ul class="footer-menu">
+              <li class="footer-menu__item"><a class="footer-menu__link" href="#1">Обмен</a></li>
+              <li class="footer-menu__item"><a class="footer-menu__link" href="#2">Поддержка</a></li>
+              <li class="footer-menu__item"><a class="footer-menu__link" href="#3">Инструкции</a></li>
+              <li class="footer-menu__item"><a class="footer-menu__link" href="#4">Сервис</a></li>
+              <li class="footer-menu__item"><a class="footer-menu__link" href="#4">Помощь</a></li>
+              <li class="footer-menu__item"><a class="footer-menu__link" href="#4">О нас</a></li>  
+            </ul>
+          </div>
+          <div class="footer-block mq-bg">
+            <span class="social__title">Мы в социальных сетях</span>
+            <div class="social-buttons">
+              <a class="social__btn vk" href="#vk">VK</a>
+              <a class="social__btn fb" href="#fb">Facebook</a>
+              <a class="social__btn inst" href="#inst">Instagram</a>
+              <a class="social__btn tw" href="#tw">Twitter</a>
+            </div>
+          </div>
+          <div class="footer-block footer-block--middle">
+            <div class="info-block">На сайте частично представлены материалы с ресурсов tricolor.tv и gs.ru.<br>Есть вопрос? Задайте его 
+              <a href="#">тут</a></div>
+            <div class="feedback">
+              <a class="feedback__link" href="#">О сайте</a>
+              <a class="feedback__link" href="#">Обратная связь</a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div> 
+    <script src="js/main.js"></script> 
+  </body>
+</html>
