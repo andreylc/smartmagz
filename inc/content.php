04c6e4a147d497cc2d0334095b298f603098f0a5
Andrey Tyapugin <andreylcm@gmail.com>
2017-01-09 00:16:32 +0300

    add load content from db

--- inc/content.php
+++ inc/content.php
@@ -5,108 +5,33 @@
         </div>
         <div class="layout flex"> <!--layout start-->
           <main class="feed flex">
-            <div class="post-container">
-              <div class="post-image">
-                <img src="img/post_1.jpg" alt="Ultra HD" width="100%">
-              </div>
-              <div class="post-body">
-                <h2 class="post__title">Новое обновление для приемника GS B520</h2>
-                <div class="post__meta meta--show">
-                  <time class="post__time" datetime="2016-12-21" title="Дата публикации">21.12.2016</time>
-                  <div class="post__category"><a class="post__category-link" href="#category" title="В разделе">Ресиверы</a></div>
-                </div>
-                <div class="post__preview-text text--show">
-                  Загрузка...
-                </div>
-                <div class="post__stats stats--show">
-                  <!--<div class="post__views"></div>
-                  <div class="post__comments"></div>-->
-                  <a class="read__more" href="#readmore">далее</a>
-                </div>
-              </div>
-            </div>
-            <div class="post-container">
-              <div class="post-image">
-                <img src="img/post_3.png" alt="Tablet" width="100%">
-              </div>
-              <div class="post-body">
-                <h2 class="post__title">Зачем менять оборудование?</h2>
-                <div class="post__meta meta--show">
-                  <time class="post__time" datetime="2016-12-20" title="Дата публикации">19.12.2016</time>
-                  <div class="post__category"><a class="post__category-link" href="#category" title="В разделе">Новости</a></div>
-                </div>
-                <div class="post__preview-text text--show">
-                  Для абсолютно всех всех абонентов «Триколор ТВ», владельцев устаревших приёмников, действуют специальные предложения обмена оборудования на современное, поддерживающее формат HD.
-                  Благодаря специальным акциям переход на новое оборудование обойдется вам значительно дешевле, чем приобретение нового комплекта за полную стоимость.
-                </div>
-                <div class="post__stats stats--show">
-                  <!--<div class="post__views">115</div>
-                  <div class="post__comments"></div>-->
-                  <a class="read__more" href="#readmore">далее</a>
-                </div>
-              </div>
-            </div>
-            <div class="post-container">
-              <div class="post-image">
-                <img src="img/post_4.jpg" alt="Tablet" width="100%">
-              </div>
-              <div class="post-body">
-                <h2 class="post__title">Мультбиблиотека от пакета "Детский"!</h2>
-                <div class="post__meta meta--show">
-                  <time class="post__time" datetime="2016-12-20" title="Дата публикации">19.12.2016</time>
-                  <div class="post__category"><a class="post__category-link" href="#category" title="В разделе">Новости</a></div>
-                </div>
-                <div class="post__preview-text text--show">
-                  Для абсолютно всех всех абонентов «Триколор ТВ», владельцев устаревших приёмников, действуют специальные предложения обмена оборудования на современное, поддерживающее формат HD.
-                  Благодаря специальным акциям переход на новое оборудование обойдется вам значительно дешевле, чем приобретение нового комплекта за полную стоимость.
-                </div>
-                <div class="post__stats stats--show">
-                  <!--<div class="post__views">115</div>
-                  <div class="post__comments"></div>-->
-                  <a class="read__more" href="#readmore">далее</a>
-                </div>
-              </div>
-            </div>
-            <div class="post-container">
-              <div class="post-image">
-                <img src="img/post_5.jpg" alt="Tablet" width="100%">
-              </div>
-              <div class="post-body">
-                <h2 class="post__title">Спутниковый интернет</h2>
-                <div class="post__meta meta--show">
-                  <time class="post__time" datetime="2016-12-20" title="Дата публикации">19.12.2016</time>
-                  <div class="post__category"><a class="post__category-link" href="#category" title="В разделе">Новости</a></div>
-                </div>
-                <div class="post__preview-text text--show">
-                  Для абсолютно всех всех абонентов «Триколор ТВ», владельцев устаревших приёмников, действуют специальные предложения обмена оборудования на современное, поддерживающее формат HD.
-                  Благодаря специальным акциям переход на новое оборудование обойдется вам значительно дешевле, чем приобретение нового комплекта за полную стоимость.
-                </div>
-                <div class="post__stats stats--show">
-                  <!--<div class="post__views">115</div>
-                  <div class="post__comments"></div>-->
-                  <a class="read__more" href="#readmore">далее</a>
-                </div>
-              </div>
-            </div>
-            <div class="post-container">
-              <div class="post-image">
-                <img src="img/post_3.jpg" alt="Tablet" width="100%">
-              </div>
-              <div class="post-body">
-                <h2 class="post__title">Акция 1000 фильмов в подарок</h2>
-                <div class="post__meta meta--show">
-                  <time class="post__time" datetime="2016-12-20" title="Дата публикации">19.12.2016</time>
-                  <div class="post__category"><a class="post__category-link" href="#category" title="В разделе">Новости</a></div>
-                </div>
-                <div class="post__preview-text text--show">
-                  Для абсолютно всех всех абонентов «Триколор ТВ», владельцев устаревших приёмников, действуют специальные предложения обмена оборудования на современное, поддерживающее формат HD.
-                  Благодаря специальным акциям переход на новое оборудование обойдется вам значительно дешевле, чем приобретение нового комплекта за полную стоимость.
-                </div>
-                <div class="post__stats stats--show">
-                  <!--<div class="post__views">115</div>
-                  <div class="post__comments"></div>-->
-                  <a class="read__more" href="#readmore">далее</a>
-                </div>
-              </div>
-            </div>
+            <?php
+            $query = "SELECT * FROM `posts` ORDER BY `post_date` DESC";
+            $result = mysqli_query($db, $query);
+            $row = mysqli_fetch_assoc($result);
+            do
+              {
+                echo ('<div class="post-container">
+                      <div class="post-image">
+                      <img src="'.$row['img']);
+                echo ('" alt="Tablet" width="100%">
+                      </div>
+                      <div class="post-body">
+                      <h2 class="post__title">'.$row['title'].'</h2>');
+                echo('<div class="post__meta meta--show">
+                      <time class="post__time" datetime="2016-12-20" title="Дата публикации">'.$row[post_date]);
+                echo('</time>
+                      <div class="post__category"><a class="post__category-link" href="#category" title="В разделе">Новости</a></div>
+                      </div>
+                      <div class="post__preview-text text--show">' .$row['post_preview'].'</div>
+                      <div class="post__stats stats--show">
+                      <!--<div class="post__views">115</div>
+                      <div class="post__comments"></div>-->
+                      <a class="read__more" href="#readmore">далее</a>
+                      </div>
+                      </div>
+                      </div>');
+            }
+          while($row = mysqli_fetch_array($result));
+          ?>
           </main>
